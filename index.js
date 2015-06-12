675d3ea92e7d93c2fc2587634ec00f1876864aed
Christian Dahlqvist <christian@elastic.com>
2015-06-12 20:18:28 +0100

    Removed update notifier.

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-/**
- * Notify user of updates
- */
-var pkg = require('./package.json');
-require('update-notifier')({ packageName: pkg.name, packageVersion: pkg.version }).notify();
 
 var manager = require('./lib/manager');
